742d458b53878c2f3a6a9407824d5cfde1112642
Sunil Band <sunilband0007@gmail.com>
2023-10-20 16:01:18 +0530

    new buttons and bold logo

--- Components/Viewer.tsx
+++ Components/Viewer.tsx
@@ -48,13 +48,12 @@ export default function Sample({ slug }: Props) {
 
   useResizeObserver(containerRef, resizeObserverOptions, onResize);
 
-
-  const [visibleMan,setVisibleMan]=useState(false)
+  const [visibleMan, setVisibleMan] = useState(false);
   function onDocumentLoadSuccess({
     numPages: nextNumPages,
   }: PDFDocumentProxy): void {
     setNumPages(nextNumPages);
-    setVisibleMan(true)
+    setVisibleMan(true);
   }
   const handleDownloadPDF = () => {
     const fileURL = `./${slug}.pdf`;
@@ -65,7 +64,7 @@ export default function Sample({ slug }: Props) {
     <div className="Example">
       <div className="head">
         <h1
-          className="text-center  tracking-wider cursive font-bold text-black"
+          className="logo cursive"
           style={{ fontSize: "30px " }}
         >
           {" "}
@@ -73,24 +72,43 @@ export default function Sample({ slug }: Props) {
         </h1>
 
         <div className="btnContainer">
-          <button onClick={handleDownloadPDF} className="dl">
-            <Image src={downloadLogo} height={30} width={30} alt="download" />
+          <button className="Btn" onClick={handleDownloadPDF}>
+            <svg
+              className="svgIcon"
+              viewBox="0 0 384 512"
+              height="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path>
+            </svg>
+            <span className="icon2"></span>
           </button>
+
           <button
-            className="contactBtn"
             onClick={() => {
               router.push("https://sunilband.netlify.app/#contact");
             }}
           >
-            Contact
+            <span className="button_top"> Contact</span>
           </button>
         </div>
       </div>
       <div className="Example__container">
-     
-        <div className="Example__container__document" ref={setContainerRef} 
-        style={{position:"relative"}}
-        > {visibleMan&&<Image src={SittingMan} width={300} height={300} alt="sittingMan" className="manSvg"/>}
+        <div
+          className="Example__container__document"
+          ref={setContainerRef}
+          style={{ position: "relative" }}
+        >
+          {" "}
+          {visibleMan && (
+            <Image
+              src={SittingMan}
+              width={300}
+              height={300}
+              alt="sittingMan"
+              className="manSvg"
+            />
+          )}
           <Document
             file={file}
             onLoadSuccess={onDocumentLoadSuccess}
